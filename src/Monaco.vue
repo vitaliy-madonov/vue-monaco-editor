4b38f8b36ec372ea5a6c4ae8ee0cb8046b460ac4
Vitaliy Madonov <vitaliy@ns-master.net>
2018-12-18 20:04:19 +0900

    fix

--- src/Monaco.vue
+++ src/Monaco.vue
@@ -14,11 +14,11 @@ module.exports = {
     srcPath: { type: String },
     language: { type: String, default: 'javascript' },
     theme: { type: String, default: 'vs-dark' }, // vs, hc-black
-    options: { type: Object, default: () => {} },
-    highlighted: { type: Array, default: () => [{
+    options: { type: Object, default: function() {return {}} },
+    highlighted: { type: Array, default: function() { return [{
       number: 0,
       class: ''
-    }] },
+    }]} },
     changeThrottle: { type: Number, default: 0 }
   },
   mounted() {
