233df95832c9d8dc33ba99d983111a88c595d4ef
Vitaliy Madonov <vitaliy@ns-master.net>
2018-12-18 19:00:59 +0900

    rebuild

--- src/MonacoLoader.js
+++ src/MonacoLoader.js
@@ -7,7 +7,7 @@ module.exports = {
     }
     const config = {
       paths: {
-        vs: srcPath + '/vs'
+        vs: '/js/monaco/vs'
       }
     };
     const loaderUrl = `${config.paths.vs}/loader.js`;
