b0ac9416ae5c8bc289a85891a026f439b5d5e1c3
Vitaliy Madonov <vitaliy@ns-master.net>
2018-12-18 20:41:03 +0900

    fix

--- src/Monaco.vue
+++ src/Monaco.vue
@@ -14,30 +14,35 @@ module.exports = {
     srcPath: { type: String },
     language: { type: String, default: 'javascript' },
     theme: { type: String, default: 'vs-dark' }, // vs, hc-black
-    options: { type: Object, default: () => {} },
-    highlighted: { type: Array, default: () => [{
-      number: 0,
-      class: ''
-    }] },
+    options: { type: Object, default: function _default() {} },
+    highlighted: { type: Array, default: function _default() {
+        return [{
+          number: 0,
+          class: ''
+        }];
+      } },
     changeThrottle: { type: Number, default: 0 }
   },
-  mounted() {
+  mounted: function mounted() {
     this.fetchEditor();
   },
-  destroyed() {
+  destroyed: function destroyed() {
     this.destroyMonaco();
   },
+
   computed: {
-    style() {
-      const { width, height } = this;
-      const fixedWidth = width.toString().indexOf('%') !== -1 ? width : `${width}px`;
-      const fixedHeight = height.toString().indexOf('%') !== -1 ? height : `${height}px`;
+    style: function style() {
+      var width = this.width,
+          height = this.height;
+
+      var fixedWidth = width.toString().indexOf('%') !== -1 ? width : width + 'px';
+      var fixedHeight = height.toString().indexOf('%') !== -1 ? height : height + 'px';
       return {
         width: fixedWidth,
-        height: fixedHeight,
+        height: fixedHeight
       };
     },
-    editorOptions() {
+    editorOptions: function editorOptions() {
       return Object.assign({}, this.defaults, this.options, {
         value: this.code,
         language: this.language,
@@ -45,7 +50,7 @@ module.exports = {
       });
     }
   },
-  data() {
+  data: function data() {
     return {
       defaults: {
         selectOnLineNumbers: true,
@@ -55,72 +60,76 @@ module.exports = {
         automaticLayout: false,
         glyphMargin: true
       }
-    }
+    };
   },
+
   watch: {
     highlighted: {
-      handler(lines) {
+      handler: function handler(lines) {
         this.highlightLines(lines);
       },
+
       deep: true
     },
-    language () {
-      window.monaco.editor.setModelLanguage(this.editor.getModel(), this.language)
+    language: function language() {
+      window.monaco.editor.setModelLanguage(this.editor.getModel(), this.language);
     }
   },
   methods: {
-    highlightLines(lines) {
+    highlightLines: function highlightLines(lines) {
+      var _this = this;
+
       if (!this.editor) {
         return;
       }
-      lines.forEach((line) => {
-        const className = line.class;
-        const highlighted = this.$el.querySelector(`.${className}`);
+      lines.forEach(function (line) {
+        var className = line.class;
+        var highlighted = _this.$el.querySelector('.' + className);
 
         if (highlighted) {
           highlighted.classList.remove(className);
         }
 
-        const number = parseInt(line.number);
-        if (!this.editor && number < 1 || isNaN(number)) {
+        var number = parseInt(line.number);
+        if (!_this.editor && number < 1 || isNaN(number)) {
           return;
         }
 
-        const selectedLine = this.$el.querySelector(`.view-lines [linenumber="${number}"]`);
+        var selectedLine = _this.$el.querySelector('.view-lines [linenumber="' + number + '"]');
         if (selectedLine) {
           selectedLine.classList.add(className);
         }
       });
     },
-    editorHasLoaded(editor, monaco) {
+    editorHasLoaded: function editorHasLoaded(editor, monaco) {
+      var _this2 = this;
+
       this.editor = editor;
       this.monaco = monaco;
-      this.editor.onDidChangeModelContent(event =>
-        this.codeChangeHandler(editor, event)
-      );
+      this.editor.onDidChangeModelContent(function (event) {
+        return _this2.codeChangeHandler(editor, event);
+      });
       this.$emit('mounted', editor);
     },
-    codeChangeHandler: function(editor) {
+
+    codeChangeHandler: function codeChangeHandler(editor) {
       if (this.codeChangeEmitter) {
         this.codeChangeEmitter(editor);
       } else {
-        this.codeChangeEmitter = debounce(
-          function(editor) {
-            this.$emit('codeChange', editor);
-          },
-          this.changeThrottle
-        );
+        this.codeChangeEmitter = debounce(function (editor) {
+          this.$emit('codeChange', editor);
+        }, this.changeThrottle);
         this.codeChangeEmitter(editor);
       }
     },
-    fetchEditor() {
+    fetchEditor: function fetchEditor() {
       monacoLoader.load(this.srcPath, this.createMonaco);
     },
-    createMonaco() {
+    createMonaco: function createMonaco() {
       this.editor = window.monaco.editor.create(this.$el, this.editorOptions);
       this.editorHasLoaded(this.editor, window.monaco);
     },
-    destroyMonaco() {
+    destroyMonaco: function destroyMonaco() {
       if (typeof this.editor !== 'undefined') {
         this.editor.dispose();
       }
