d0140c1f33d7eb055df6ec7f7babcdbc776efbc8
Vitaliy Madonov <vitaliy@ns-master.net>
2018-12-18 16:15:34 +0900

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'vue-monaco-editor.js'
   },
   module: {
     rules: [
